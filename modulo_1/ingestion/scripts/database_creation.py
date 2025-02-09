69b8db475e7664173663a4e8e96fa0fdc1eadfb6
Lucas Rodrigues Schiavetti <lurvetti@gmail.com>
2025-02-09 14:41:22 -0300

    go

--- modulo_1/ingestion/scripts/database_creation.py
+++ modulo_1/ingestion/scripts/database_creation.py
@@ -1,6 +1,6 @@
 # creating a database on the data directory, parsing a name argument
-
 # imports
+
 import os
 import argparse
 from utils import get_directories, create_db
@@ -22,9 +22,6 @@ def main():
     print(f'\nCreating database {database} ...')
     database_path = os.path.join(DATA_DIR, database)
     create_db(database_path)
-
-    variable_1 = "testin"
-    variable_2 = 26
 
 if __name__ == "__main__":
     main()
